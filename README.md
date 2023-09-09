11a0be54e971e0e65a367d7e597995600ff27c08
teejas <tejas97siripurapu@gmail.com>
2023-09-09 13:53:58 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Introduction
+
+A simple CLI tool, which when given a directory will search it for audio files and play them back.
+
+## Steps
+1. Build simple command line arg parser, should just take one flag (a directory)
+2. Search directory for audio files (start with hardcoding search for .mp3 and .wav files)
+3. Playback using `hound` or smn
+4. Allow user to control playback, pause and skip through files in the directory
+5. Build some kind of GUI/TUI to show user what song is playing and progress
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::{
+    error::Error,
+    fs,
+    path::PathBuf
+};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// The path to the directory to search for audio files in.
+    #[arg(short, long)]
+    dir: PathBuf,
+}
+
+fn parse_args(args: Cli) -> PathBuf {
+    args.dir
+}
+
+// searches directory and returns a vector of audio pathbufs
+fn search_directory(dir: PathBuf) -> Result<Vec<PathBuf>, Box<dyn Error>> { 
+    let mut ret: Vec<PathBuf> = Vec::new();
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        let metadata = fs::metadata(&path)?;
+        if metadata.is_file() {
+            if let Some(ext) = path.extension() {
+                println!("{:?}", ext);
+                if ext == "wav" || ext == "mp3" {
+                    ret.push(path);
+                }
+            }
+        }
+    }
+    Ok(ret)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let dir = parse_args(Cli::parse());
+    let audiofiles: Vec<PathBuf> = search_directory(dir)?;
+    println!("{:?}", audiofiles);
+    Ok(())
+}
+
+mod tests {
+    use super::*;
+    #[test]
+    fn search_dir_with_no_audio_files() {
+        let ret = search_directory(PathBuf::from(".")).unwrap();
+        let check: Vec<PathBuf> = Vec::new();
+        assert_eq!(ret, check);
+    }
+    #[test]
+    fn search_dir_with_one_audio_file() {
+        let mut ret = search_directory(PathBuf::from("examples/")).unwrap();
+        ret.sort();
+        assert_eq!(ret, 
+            vec![
+                PathBuf::from("examples/mypianothingilike.mp3"), 
+                PathBuf::from("examples/shorter_cc_hornlinez.wav")
+            ]
+        );
+    }
+}
