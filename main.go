3b921ad29708469453f4e9b613ac2582e42721c8
teejas <tejas97siripurapu@gmail.com>
2023-09-21 13:35:57 -0700

    add fetchSongs()

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
@@ -10,13 +11,32 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func fetchSongs(dir string) ([]fs.DirEntry, error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+		return entries, err
+	}
+	return entries, nil
+}
+
 func startServer(c *cli.Context) error {
 	fmt.Printf("dir: %s\n", c.String("dir"))
+	songs, err := fetchSongs(c.String("dir"))
+	if err != nil {
+		log.Fatal("Failed to fetch songs")
+		return err
+	}
 	fmt.Println("Starting HTTP server...")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": songs[0].Name(),
 		})
 	})
 	r.Run()
